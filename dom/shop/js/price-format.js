83e732f5cba1885efbcbe1f6c8b3625ac7b9568f
Aleksey Konotop <alekseykonotop@gmail.com>
2018-12-08 01:50:26 +0300

    Price format is done

--- dom/shop/js/price-format.js
+++ dom/shop/js/price-format.js
@@ -1,3 +1,21 @@
 function getPriceFormatted(value) {
   return  value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 }
+
+function init() {
+  const cart = document.getElementsByClassName('cart')[0];
+  let [counter, totalPayment] = cart.querySelectorAll('div.head span');
+  totalPayment.setAttribute('total-price', '0');
+
+  const addButtons = document.getElementsByClassName('add');
+  Array.from(addButtons).forEach(addButton => 
+    addButton.addEventListener('click', () => {
+      const price = addButton.getAttribute('data-price');
+      counter.innerHTML++;
+      totalPayment.setAttribute('total-price', `${+totalPayment.getAttribute('total-price') + +price}`);
+      totalPayment.innerHTML = getPriceFormatted(totalPayment.getAttribute('total-price'));
+    }));
+
+}
+
+document.addEventListener('DOMContentLoaded', init);
