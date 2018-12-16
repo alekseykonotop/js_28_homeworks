93c54189df65d170eb4ab62087686dd382f038fe
Aleksey Konotop <alekseykonotop@gmail.com>
2018-12-17 00:50:32 +0300

    Todo-list is done, but the google fonts are not loaded.

---
+++ html-document-structure/todo-list/js/todo.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const todoList = document.querySelector('.todo-list');
+const done = todoList.querySelector('.done');
+const undone = todoList.querySelector('.undone');
+const tasks = todoList.querySelectorAll('input');
+
+
+function upgradeStatus() {
+    this.checked = this.checked ? false : true;
+    
+    const checkedField = this.parentNode;
+    checkedField.parentNode.removeChild(checkedField);
+    this.checked ? undone.appendChild(checkedField) : done.appendChild(checkedField);
+    
+    const inputField = checkedField.querySelector('input');
+    inputField.addEventListener('click', upgradeStatus);
+    inputField.checked = this.checked ? false : true;
+}
+
+ Array.from(tasks).forEach(task => {
+    task.addEventListener('click', upgradeStatus);
+})
