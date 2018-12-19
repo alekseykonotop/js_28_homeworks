3b9bbeef15c9137a54f7f8735ad0d9046bd4a52a
Aleksey Konotop <alekseykonotop@gmail.com>
2018-12-19 23:50:26 +0300

    Table is done

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    if (event.target.tagName !== 'TH') {
+        return;
+    }
+    event.target.dataset.dir = (event.target.dataset.dir < 0 || !event.target.dataset.dir) ? 1 : -1;
+    event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+    
+    sortTable(event.currentTarget.dataset.sortBy, event.target.dataset.dir);
 }
