fe9aa08e946285862756a03a6e13e186e3e4b78d
Aleksey Konotop <alekseykonotop@gmail.com>
2018-12-20 16:16:46 +0300

    Shop is done

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,13 @@
 'use strict';
+
+list.addEventListener('click', ( event ) => {
+    if (event.target.tagName !== 'A') {
+        return;
+    }
+    const item = {
+        title: event.target.dataset.title,
+        price: event.target.dataset.price
+    }
+    addToCart(item);
+
+})
