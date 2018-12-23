2c5d0796abc239c424680173284fad1464d31c41
Aleksey Konotop <alekseykonotop@gmail.com>
2018-12-23 18:19:02 +0300

    Counter is done

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const counter = document.querySelector('.wrap');
+const currentCounter = counter.querySelector('#counter');
+const buttons = counter.querySelector('.wrap-btns')
+
+function heandler(event) {
+    switch (event.target.id) {
+        case 'increment':
+            localStorage.currentValue = Number(localStorage.currentValue) + 1;
+            currentCounter.innerHTML = localStorage.currentValue;
+
+            break;
+
+        case 'decrement':
+            if (Number(localStorage.currentValue) > 0) {
+                localStorage.currentValue = Number(localStorage.currentValue) - 1;
+                currentCounter.innerHTML = localStorage.currentValue;
+            }
+            break;
+
+        case 'reset':
+            localStorage.currentValue = 0;
+            currentCounter.innerHTML = localStorage.currentValue;
+            break;
+
+    }
+}
+
+if (!localStorage.currentValue) {
+    localStorage.currentValue = 0;
+}
+currentCounter.innerHTML = localStorage.currentValue;
+buttons.addEventListener('click', heandler);
