5780dd109855ef85cde28c472e2bd9bb5368d717
Aleksey Konotop <alekseykonotop@gmail.com>
2018-12-26 00:57:21 +0300

    Counter is done

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function init() {
+    const counterHtml = document.querySelector('.counter');
+    const outputErrors = document.querySelector('.errors');
+    const connection = new WebSocket('wss://neto-api.herokuapp.com/counter');
+    connection.addEventListener('message', event => {
+        const data = JSON.parse(event.data);
+        counterHtml.innerHTML = `${data.connections}`;
+        outputErrors.innerHTML = `${data.errors}`;
+    });
+
+    window.addEventListener('beforeunload', () => {
+        connection.close(1000);
+    });
+
+}
+
+
+document.addEventListener('DOMContentLoaded', init);
