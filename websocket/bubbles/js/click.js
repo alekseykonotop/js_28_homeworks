84268b6805f962e3ef51355f90a4634531751e12
Aleksey Konotop <alekseykonotop@gmail.com>
2018-12-26 01:34:46 +0300

    Bubbles are done

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const connection = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+connection.addEventListener('open', () => showBubbles(connection));
+
+function sendWs(event) {
+    connection.send(JSON.stringify({
+        x: event.x,
+        y: event.y
+    }));
+    
+    connection.addEventListener('error', error => console.log(`Ошибка: ${error.data}`)
+    );
+}
+
+document.addEventListener('click', sendWs);
