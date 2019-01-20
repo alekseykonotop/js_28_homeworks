4694f428dd356dba642d68e5358ff3ebf9973de9
Aleksey Konotop <alekseykonotop@gmail.com>
2019-01-20 16:33:44 +0300

    Audioplayer is done

--- html-element-collection/audioplayer/audioplayer.js
+++ html-element-collection/audioplayer/audioplayer.js
@@ -1,57 +1,72 @@
 'use strict'
 
-const songTitles = [
-    'LA Chill Tour.mp3',
-    'LA Fusion Jam.mp3',
-    'This is it band.mp3'
-];
-
 const songs = {
     'LA Chill Tour.mp3': 'http://d.zaix.ru/9Hec.mp3',
     'LA Fusion Jam.mp3': 'http://d.zaix.ru/9Hed.mp3',
     'This is it band.mp3': 'http://d.zaix.ru/9Hee.mp3'
 };
 
-let songIndex = 1;
+const songTitles = Object.keys(songs);
+const mediaplayer = document.getElementsByClassName('mediaplayer')[0];
 const player = document.getElementsByTagName('audio')[0];
 player.src = songs[songTitles[0]];
 const btnState = document.getElementsByClassName('playstate')[0];
-const songTitle = document.getElementsByTagName('span')[0];
+const songTitle = document.getElementsByClassName('title')[0];
 
-function playAndPause() {
-    player.paused ? player.play() : player.pause();
+let songIndex = 0;
+
+function setActualIndex(isIncrease) {
+    if (isIncrease) {
+        songIndex = (songIndex + 1 + songTitles.length) % songTitles.length;
+        return;
+    }
+    songIndex = (songIndex - 1 + songTitles.length) % songTitles.length;
+}
+
+function showCurrentTitle() {
+    songTitle.title = songTitles[songIndex];
+}
+
+function play() {
+    player.src = songs[songTitles[songIndex]];
+    mediaplayer.classList.contains('play') ? player.pause(): player.play();
+    mediaplayer.classList.toggle('play');
+    showCurrentTitle();
 }
 
 function stop() {
     player.pause();
     player.currentTime = 0;
     btnPlay.classList.remove('is-playing');
-}
-
-function playCurrentTrack(ind) {
-    const currentIndex = (ind + songTitles.length) % songTitles.length;
-    player.src = songs[songTitles[currentIndex]];
-    songIndex = currentIndex;
-    player.play();
-    songTitle.title =  songTitles[songIndex];
-}
-
-function back() {
-    playCurrentTrack(songIndex - 1);
+    mediaplayer.classList.remove('play');
 }
 
 function next() {
-    playCurrentTrack(songIndex + 1);
+    setActualIndex(true);
+    if (!mediaplayer.classList.contains('play')) {
+        showCurrentTitle();
+        return;
+    }
+    mediaplayer.classList.remove('play');
+    play();
 }
 
+function back() {
+    setActualIndex(false);
+    if (!mediaplayer.classList.contains('play')) {
+        showCurrentTitle();
+        return;
+    }
+    mediaplayer.classList.remove('play');
+    play();
+}
 
-const btnPlay = document.getElementsByClassName('fa-play')[0];
-const btnStop = document.getElementsByClassName('fa-stop')[0];
-const btnBack = document.getElementsByClassName('fa-backward')[0];
-const btnNext = document.getElementsByClassName('fa-forward')[0];
+const btnPlay = document.getElementsByClassName('playstate')[0];
+const btnStop = document.getElementsByClassName('stop')[0];
+const btnBack = document.getElementsByClassName('back')[0];
+const btnNext = document.getElementsByClassName('next')[0];
 
-
-btnPlay.onclick = playAndPause;
+btnPlay.onclick = play;
 btnStop.onclick = stop;
 btnBack.onclick = back;
 btnNext.onclick = next;
