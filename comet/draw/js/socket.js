a708a0699e5e4f674c2b77f162285b601dfad0aa
Aleksey Konotop <alekseykonotop@gmail.com>
2019-01-21 16:15:05 +0300

    Draw is done

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+function init() {
+    const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+    ws.addEventListener('open', () => {
+        
+        editor.addEventListener('update', (event) => {
+            event.canvas.toBlob(blob => ws.send(blob));
+
+        });
+    });
+
+    ws.addEventListener('message', event => {
+        console.log(`Сессия # ${event.data}`);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', init);
