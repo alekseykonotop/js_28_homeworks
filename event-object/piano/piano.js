cf8d2eab6907bc553bf778ca6f7d8cfda4abbe6f
Aleksey Konotop <alekseykonotop@gmail.com>
2019-01-22 23:36:35 +0300

    Piano is done

--- event-object/piano/piano.js
+++ event-object/piano/piano.js
@@ -6,14 +6,40 @@ const songs = ['first.mp3', 'second.mp3', 'third.mp3', 'fourth.mp3', 'fifth.mp3'
 const piano = document.getElementsByTagName('ul')[0];
 const buttons = piano.getElementsByTagName('li');
 
-let soundsTone = 'middle';
+let currentTone = 'middle';
+const tones = ['lower', 'middle', 'higher'];
 
 function setPathsOfSounds() {
     for (let i = 0; i < buttons.length; i++) {
         const player = buttons[i].getElementsByTagName('audio')[0];
-        player.src = `${mainFolder}\/${soundsTone}\/${songs[i]}`;
+        player.src = `${mainFolder}\/${currentTone}\/${songs[i]}`;
     }
 }
+
+function setPianoClass() {
+    tones.forEach(tone => {
+        if (piano.classList.contains(tone)) {
+            piano.classList.remove(tone)
+        }
+    });
+    piano.classList.add(currentTone);
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.shiftKey) {
+        currentTone = 'lower';
+    }
+
+    if (event.altKey) {
+        currentTone = 'higher';
+    }
+    setPianoClass();
+}, false);
+
+document.addEventListener('keyup', (event) => {
+    currentTone = 'middle';
+    setPianoClass();
+}, false);
 
 Array.from(buttons).forEach(button => {
     const player = button.getElementsByTagName('audio')[0];
@@ -23,19 +49,7 @@ Array.from(buttons).forEach(button => {
     });
 });
 
-document.addEventListener('keydown', (event) => {
-    if (event.shiftKey) {
-        soundsTone = 'lower';
-    }
-
-    if (event.altKey) {
-        soundsTone = 'higher';
-    }
-}, false);
-
-document.addEventListener('keyup', (event) => {
-    soundsTone = 'middle';
-}, false);
+setPianoClass();
 
 
 
